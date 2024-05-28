972ba5407aca0a5367270f2c1f7a0bd8c9c1c12b
josiahabranner <92758458+josiahabranner@users.noreply.github.com>
2024-05-28 18:07:47 -0400

    "get" use case

---
+++ Frontpoint.UseCases/Individuals/Get/GetIndiviualHandler.cs
@@ -0,0 +1,25 @@
+﻿using Ardalis.Result;
+using Ardalis.SharedKernel;
+using Frontpoint.Core.Entities.IndividualAggregate;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Frontpoint.UseCases.Individuals.Get;
+
+public class GetIndividualHandler(IRepository<Individual> repository, ILogger<GetIndividualHandler> logger) : IRequestHandler<GetIndividualQuery, Result<IndividualDto>>
+{
+    private readonly IRepository<Individual> _repository = repository;
+    private readonly ILogger<GetIndividualHandler> _logger = logger;
+
+    public async Task<Result<IndividualDto>> Handle(GetIndividualQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _repository.GetByIdAsync(request.IndividualId, cancellationToken);
+        if (result == null)
+        {
+            _logger.LogWarning("GetByIdAsync({Id}) not found", request.IndividualId);
+            return Result.NotFound();
+        }
+
+        return Result.Success(IndividualDto.FromEntity(result));
+    }
+}
---
+++ Frontpoint.UseCases/Individuals/Get/GetIndiviualQuery.cs
@@ -0,0 +1,10 @@
+﻿using Ardalis.Result;
+using MediatR;
+
+namespace Frontpoint.UseCases.Individuals.Get;
+
+/// <summary>
+/// Get an individual
+/// </summary>
+/// <param name="IndividualId"></param>
+public record GetIndividualQuery(int IndividualId) : IRequest<Result<IndividualDto>>;
