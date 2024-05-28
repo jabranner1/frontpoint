7c77c28ccfd28a0d840b07c3ffd01d1a1af66247
josiahabranner <92758458+josiahabranner@users.noreply.github.com>
2024-05-28 18:07:58 -0400

    "list" use case

---
+++ Frontpoint.UseCases/Individuals/List/ListIndiviualsHandler.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Result;
+using Ardalis.SharedKernel;
+using Frontpoint.Core.Entities.IndividualAggregate;
+using Frontpoint.Core.Entities.IndividualAggregate.Specifications;
+using MediatR;
+
+namespace Frontpoint.UseCases.Individuals.List;
+
+public class ListIndividualsHandler(IRepository<Individual> repository) : IRequestHandler<ListIndividualsQuery, Result<IEnumerable<IndividualDto>>>
+{
+    private readonly IRepository<Individual> _repository = repository;
+
+    public async Task<Result<IEnumerable<IndividualDto>>> Handle(ListIndividualsQuery request, CancellationToken cancellationToken)
+    {
+        var spec = new PaginateIndiviualsSpec(request.Take, request.Skip);
+        var result = (await _repository.ListAsync(spec, cancellationToken)) ?? [];
+        return Result.Success(result.Select(IndividualDto.FromEntity));
+    }
+}
---
+++ Frontpoint.UseCases/Individuals/List/ListIndiviualsQuery.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.Result;
+using MediatR;
+
+namespace Frontpoint.UseCases.Individuals.List;
+
+/// <summary>
+/// List individuals
+/// </summary>
+/// <param name="Take"></param>
+/// <param name="Skip"></param>
+public record ListIndividualsQuery(int? Take, int? Skip) : IRequest<Result<IEnumerable<IndividualDto>>>;
