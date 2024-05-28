c361803179c32d01288e4f2696a243a9b7ca10b0
josiahabranner <92758458+josiahabranner@users.noreply.github.com>
2024-05-28 18:06:29 -0400

    Create IndividualRepository.cs

---
+++ Frontpoint.Infrastructure/Repositories/IndividualRepository.cs
@@ -0,0 +1,10 @@
+﻿using Ardalis.SharedKernel;
+using Ardalis.Specification.EntityFrameworkCore;
+using Frontpoint.Core.Entities.IndividualAggregate;
+using Frontpoint.Infrastructure.Data;
+
+namespace Frontpoint.Infrastructure.Repositories;
+
+public class IndividualRepository(FrontpointDbContext dbContext) : RepositoryBase<Individual>(dbContext), IRepository<Individual>
+{
+}
