78668893ed537624bef6f0a8a8da5373d395ac3f
josiahabranner <92758458+josiahabranner@users.noreply.github.com>
2024-05-28 18:24:14 -0400

    Create individual.ts

---
+++ frontpoint.client/src/api/models/individual.ts
@@ -0,0 +1,15 @@
+export interface Individual {
+  id?: number;
+  prefix?: string;
+  firstName?: string;
+  middleName?: string;
+  lastName?: string;
+  dateOfBirth?: Date;
+  telephoneNumber?: string;
+  addressLine1?: string;
+  addressLine2?: string;
+  city?: string;
+  state?: string;
+  zip?: string;
+  country?: string;
+}
