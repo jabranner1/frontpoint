66c9e6f888db59d18a15b8cad3757bcafe2b55f4
josiahabranner <92758458+josiahabranner@users.noreply.github.com>
2024-05-28 18:11:21 -0400

    Frontpoint.Server scaffold cleanup

--- Frontpoint.Server/Program.cs
+++ Frontpoint.Server/Program.cs
@@ -1,9 +1,25 @@
+using Ardalis.SharedKernel;
+using Frontpoint.Core;
+using Frontpoint.Core.Entities.IndividualAggregate;
+using Frontpoint.Infrastructure;
+using Frontpoint.Infrastructure.Data;
+using Frontpoint.UseCases.Individuals;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+builder.Services.AddSerilog();
+
+builder.Services.AddCoreServices();
+builder.Services.AddInfrastructureServices(builder.Configuration.GetConnectionString("SqliteConnection"));
+
+ConfigureMediatR();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -12,7 +28,6 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -27,4 +42,38 @@ app.MapControllers();
 
 app.MapFallbackToFile("/index.html");
 
+SeedDatabase(app);
+
 app.Run();
+
+void ConfigureMediatR()
+{
+    var mediatRAssemblies = new[]
+    {
+    Assembly.GetAssembly(typeof(Individual)), // Core
+    Assembly.GetAssembly(typeof(IndividualDto)), // UseCases
+    Assembly.GetAssembly(typeof(InfrastructureServiceExtensions)) // Infrastructure
+  };
+
+    builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(mediatRAssemblies!));
+    builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+    builder.Services.AddScoped<IDomainEventDispatcher, MediatRDomainEventDispatcher>();
+}
+
+static void SeedDatabase(WebApplication app)
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<FrontpointDbContext>();
+        context.Database.Migrate();
+        SeedData.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred seeding the DB. {exceptionMessage}", ex.Message);
+    }
+}
