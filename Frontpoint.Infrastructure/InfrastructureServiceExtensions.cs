84cb3fd347852ee42bb741b10392e5749212f9f0
josiahabranner <92758458+josiahabranner@users.noreply.github.com>
2024-05-28 18:06:36 -0400

    Create InfrastructureServiceExtensions.cs

---
+++ Frontpoint.Infrastructure/InfrastructureServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Ardalis.GuardClauses;
+using Ardalis.SharedKernel;
+using Frontpoint.Core.Entities.IndividualAggregate;
+using Frontpoint.Infrastructure.Data;
+using Frontpoint.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Frontpoint.Infrastructure;
+
+public static class InfrastructureServiceExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(
+      this IServiceCollection services,
+      string? connectionString)
+    {
+        Guard.Against.NullOrEmpty(connectionString);
+
+        services.AddDbContext<FrontpointDbContext>(options => options.UseSqlite(connectionString));
+
+        services.AddScoped(typeof(IRepository<Individual>), typeof(IndividualRepository));
+
+        return services;
+    }
+}
